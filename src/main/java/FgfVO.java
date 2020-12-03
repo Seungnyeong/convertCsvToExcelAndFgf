aee9dc9a35a0e1f3ab7f1dd4b46d7f8011db9822
seungnyeong.kim <mellow0401@naver.com>
2020-12-04 00:02:20 +0900

    Apply FgfVO String.class to StringBuilder Object

--- src/main/java/FgfVO.java
+++ src/main/java/FgfVO.java
@@ -1,52 +1,59 @@
+import java.lang.reflect.Field;
 import java.util.UUID;
 
 public class FgfVO {
-    private String pk;
-    private String sentense;
-    private String category_l;
-    private String category_m;
-    private String category_s;
+    private StringBuilder pk;
+    private StringBuilder sentense;
+    private StringBuilder category_l;
+    private StringBuilder category_m;
+    private StringBuilder category_s;
+    private Field[] field =   FgfVO.class.getDeclaredFields();
 
-
+    public FgfVO ()  {
+        this.pk = new StringBuilder();
+        this.sentense = new StringBuilder();
+        this.category_l = new StringBuilder();
+        this.category_m = new StringBuilder();
+        this.category_s = new StringBuilder();
+    }
     public String getPk() {
-        return pk;
+        return pk.toString();
     }
 
     public void setPk(String pk) {
-        this.pk = "<__no__>" + pk;
+        this.pk.insert(0,"<__"+ field[0].getName() +"__>").append(pk);
     }
 
     public String getSentense() {
-        return sentense;
+        return sentense.toString();
     }
 
     public void setSentense(String sentense) {
-        this.sentense = "<__sentense__>" + sentense;
+        this.sentense.insert(0,"<__"+ field[1].getName() +"__>").append(sentense);
     }
 
     public String getCategory_l() {
-        return category_l;
+        return category_l.toString();
     }
 
     public void setCategory_l(String category_l) {
-        this.category_l = "<__cls_l__>" + category_l;
+        this.category_l.insert(0,"<__"+ field[2].getName() +"__>").append(category_l);
     }
 
     public String getCategory_m() {
-        return category_m;
+        return category_m.toString();
     }
 
     public void setCategory_m(String category_m) {
-        this.category_m = "<__cls_m__>" +  category_m;
+        this.category_m.insert(0,"<__"+ field[3].getName() +"__>").append(category_m);
     }
 
     public String getCategory_s() {
-        return category_s;
+        return category_s.toString();
     }
 
     public void setCategory_s(String category_s) {
-        this.category_s = "<__cls_s__>" + category_s;
+        this.category_s.insert(0, "<__"+ field[4].getName() +"__>").append(category_s);
     }
-
 
 }
